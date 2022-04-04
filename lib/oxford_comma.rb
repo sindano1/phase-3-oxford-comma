d6137be7bf3422d8a511b940822dc5f5fa1f3ddc
Sarah Indano <sarah.indano@gmail.com>
2022-04-04 16:37:18 -0400

    commit tree 5993c0e33f11004ee2feeeae28681de29d769f89

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+require 'pry'
 
-end
+def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
+
+    array[-1] = "and #{array[-1]}"
+
+    array.join(", ")  
+  end
+
